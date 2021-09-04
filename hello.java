7a9565f521ee7ec46d04a78fe859f2a7ce96ae9e
史先生 <87529060+shi-rong-zheng@users.noreply.github.com>
2021-09-04 23:39:20 +0800

    Add files via upload

---
+++ hello.java
@@ -0,0 +1,18 @@
+/*
+public class hello {
+    public static void main(String[] args)
+    {
+        System.out.println("hello,world!!\nshirongzheng");
+    }
+
+}
+*/
+public class hello{
+    public static void main(String [] args) {
+        byte a;
+        int b=298;
+        a=(byte)b;
+        System.out.println("b="+b);
+        System.out.println("a="+a);
+    }
+}
